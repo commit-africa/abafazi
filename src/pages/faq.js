c54b878b41a23824fab46ffd248ab47747d63708
aleib <>
2019-07-18 12:53:34 +0200

    faq

---
+++ src/pages/faq.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Layout from 'components/layout';
+import Box from 'components/box';
+import Head from 'components/head';
+
+const FAQ = () => (
+  <Layout>
+    <Head pageTitle="FAQs" />
+    <Box>
+      <div>123</div>
+    </Box>
+  </Layout>
+);
+
+FAQ.propTypes = {
+  // data: PropTypes.object.isRequired,
+};
+
+export default FAQ;
