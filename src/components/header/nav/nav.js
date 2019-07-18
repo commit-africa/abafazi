300e07567d7b6d7c26cf501740454428f50f9403
aleib <>
2019-07-18 17:24:10 +0200

    add icons

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Facebook, Twitter } from 'styled-icons/fa-brands';
 import { Link } from 'gatsby';
 import { Container } from './nav.css';
+import ExternalLink from 'components/links/external';
+
+const FacebookLink = styled(Facebook)`
+  color: black;
+`;
+
+const TwitterLink = styled(Twitter)`
+  color: black;
+`;
 
 const Nav = () => (
   <Container>
@@ -16,6 +27,16 @@ const Nav = () => (
       </li>
       <li>
         <a href="https://github.com/diff-community/abafazi">GitHub</a>
+      </li>
+      <li>
+        <ExternalLink url="https://twitter.com/nsm_za">
+          <TwitterLink size="26" />
+        </ExternalLink>
+      </li>
+      <li>
+        <ExternalLink url="https://www.facebook.com/NationalShelterMovementSA/">
+          <FacebookLink size="26" />
+        </ExternalLink>
       </li>
     </ul>
   </Container>
