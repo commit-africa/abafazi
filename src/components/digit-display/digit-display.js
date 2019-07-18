755bc4993628fc7112978dd6903e10e8c2037c06
Vic <victor.kirov@gmail.com>
2019-07-18 18:26:12 +0200

    Added visibility check on digit display

--- src/components/digit-display/digit-display.js
+++ src/components/digit-display/digit-display.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import IO from 'components/io';
+
 import { Container } from './digit-display.css';
 import { useInterval } from 'hooks/timing';
 
-const DigitDisplay = ({ value: targetValue, scale }) => {
+const DigitDisplay = ({ value: targetValue, scale, hasBeenVisible }) => {
   const [value, setValue] = useState(0);
 
   const distance = Math.abs(targetValue - value);
@@ -12,7 +14,11 @@ const DigitDisplay = ({ value: targetValue, scale }) => {
   const delay = distancePerc > 60 ? 2 : 50;
 
   useInterval(() => {
-    const increment = Math.min(Math.ceil(distance / 10), distance);
+    if (!hasBeenVisible) return;
+    const increment = Math.min(
+      Math.ceil(distance / Math.min(10, targetValue)),
+      distance
+    );
     if (value < targetValue) setValue(value + increment);
     else if (value > targetValue) setValue(value - increment);
   }, delay);
@@ -23,6 +29,20 @@ const DigitDisplay = ({ value: targetValue, scale }) => {
 DigitDisplay.propTypes = {
   value: PropTypes.number,
   scale: PropTypes.number,
+  hasBeenVisible: PropTypes.bool,
 };
 
-export default DigitDisplay;
+const VisibilityWrappedDigitDisplay = props => (
+  <IO rootMargin="-10px">
+    {({ hasBeenVisible }) => (
+      <DigitDisplay {...props} hasBeenVisible={hasBeenVisible} />
+    )}
+  </IO>
+);
+
+VisibilityWrappedDigitDisplay.propTypes = {
+  value: PropTypes.number,
+  scale: PropTypes.number,
+};
+
+export default VisibilityWrappedDigitDisplay;
