a664e2a252059458a11d4a2e246cf2d2e65fc49e
aleib <>
2019-07-18 17:42:18 +0200

    removing io example

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
 import Gallery from 'components/gallery';
-import IOExample from 'components/io-example';
 import Modal from 'containers/modal';
 import { graphql } from 'gatsby';
 
@@ -38,8 +37,6 @@ const Index = ({ data }) => (
     <ResourceSection />
 
     <Gallery items={data.homeJson.gallery} />
-    <div style={{ height: '50vh' }} />
-    <IOExample />
   </Layout>
 );
 
