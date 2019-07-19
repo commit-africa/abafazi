8c98379e5a314d7354bb7aaa93c177d509cf9b41
Mark Turner <animark1024@gmail.com>
2019-07-19 07:42:06 +0200

    Remove Query to pass Tests for now

--- src/sections/header.js
+++ src/sections/header.js
@@ -1,28 +1,13 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
-// import MEDIA from 'helpers/mediaTemplates';
-import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import Section from '../components/section';
 import { Container } from '../components/layout/container.css';
 import { SectionTitle } from '../components/typography/titles';
 
 const HeaderSection = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        homeJson {
-          header
-        }
-      }
-    `
-  )
 
   return(
     <Section background="#e9eef0">
-      <Img fluid={data.homeJson.header} alt='title' />
       <Container>
         <SectionTitle>The Story</SectionTitle>
       </Container>
